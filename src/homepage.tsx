2b0a6e7dcf395c636a93e2f2af06f8d875417b93
huanhaochang <raphaelhhc@gmail.com>
2024-04-19 19:46:03 +0900

    first deploy

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -14,13 +14,11 @@ function Homepage() {
     const [day_span, setDaySpan] = useState<number>(1);
     const [resultData, setResultData] = useState(null);
     const [resultloading, setResultLoading] = useState(false);
-
-    const [testData, setTestData] = useState(null);
-    const [testloading, setTestLoading] = useState(false);
     
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "",
         libraries,
+        language: 'en'
     });
 
     const onLoad = (autoC: google.maps.places.Autocomplete) => {
@@ -39,7 +37,6 @@ function Homepage() {
     const cleanText = () => {
         setInputValue("");
         setPlace(null);
-        setTestData(null);
     }
 
     const run = async () => {
@@ -47,32 +44,52 @@ function Homepage() {
             alert('Please select a place using the autocomplete input.');
             return;
         }
-        if (day_span < 1 || day_span > 60) {
+        if (!day_span || day_span < 1) {
+            alert('Please enter a valid TRAVEL DAY SPAN number.');
+            return;
+        }
+        if (day_span > 60) {
             alert('TRAVEL DAY SPAN must be between 1 and 60.');
             return;
         }
+        setResultData(null);
         await getResultData();
     }
 
     const getResultData = async () => {
         try {
             setResultLoading(true);
-            const rain_volume_lists: number[][] = await getRainVolumeLists();
-            const temperature_lists: number[][] = await getTemperatureLists();
-            const [rain_volume_probabilities, rain_volume_scores] = await getRainVolumeProbabilityScore(rain_volume_lists);
-            const [temperature_probabilities, temperature_scores] = await getTemperatureProbabilityScore(temperature_lists);
-            console.log('data collect & analysis done!')
-            const response = await axios.post('http://127.0.0.1:5000/recommend', {
+            const checkresult = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/recommend/cache`, {
                 place: place?.name,
                 this_year: this_year,
                 past_span: past_span,
-                day_span: day_span,
-                rain_volume_probabilities: rain_volume_probabilities,
-                temperature_probabilities: temperature_probabilities,
-                rain_volume_scores: rain_volume_scores,
-                temperature_scores: temperature_scores
+                day_span: day_span
             });
-            setResultData(response.data);
+            if (checkresult.data) {
+                setResultData(checkresult.data);
+            } else {
+
+                const rain_volume_lists = await getRainVolumeLists();
+                const temperature_lists = await getTemperatureLists();
+
+                if (rain_volume_lists && temperature_lists) {
+                    const [rain_volume_probabilities, rain_volume_scores] = await getRainVolumeProbabilityScore(rain_volume_lists);
+                    const [temperature_probabilities, temperature_scores] = await getTemperatureProbabilityScore(temperature_lists);
+                    console.log('data collect & analysis done!')
+
+                    const response = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/recommend`, {
+                        place: place?.name,
+                        this_year: this_year,
+                        past_span: past_span,
+                        day_span: day_span,
+                        rain_volume_probabilities: rain_volume_probabilities,
+                        temperature_probabilities: temperature_probabilities,
+                        rain_volume_scores: rain_volume_scores,
+                        temperature_scores: temperature_scores
+                    });
+                    setResultData(response.data);
+                }
+            }
             setResultLoading(false);
         } catch (error) {
             console.error('Error fetching data: ', error);
@@ -82,7 +99,7 @@ function Homepage() {
 
     const getRainVolumeLists = async () => {
         try {
-            const response = await axios.post('http://127.0.0.1:5000/rainvolumecollector', {
+            const response = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/rainvolumecollector`, {
                 place: place?.name,
                 lat: place?.geometry?.location?.lat(),
                 lon: place?.geometry?.location?.lng(),
@@ -103,7 +120,7 @@ function Homepage() {
 
     const getTemperatureLists = async () => {
         try {
-            const response = await axios.post('http://127.0.0.1:5000/temperaturecollector', {
+            const response = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/temperaturecollector`, {
                 place: place?.name,
                 lat: place?.geometry?.location?.lat(),
                 lon: place?.geometry?.location?.lng(),
@@ -124,7 +141,7 @@ function Homepage() {
 
     const getRainVolumeProbabilityScore = async (rain_volume_lists: number[][]) => {
         try {
-            const response = await axios.post('http://127.0.0.1:5000/rainvolumeanalyzer', {
+            const response = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/rainvolumeanalyzer`, {
                 place: place?.name,
                 this_year: this_year,
                 past_span: past_span,
@@ -147,7 +164,7 @@ function Homepage() {
 
     const getTemperatureProbabilityScore = async (temperature_lists: number[][]) => {
         try {
-            const response = await axios.post('http://127.0.0.1:5000/temperatureanalyzer', {
+            const response = await axios.post(`${process.env.REACT_APP_RESTAPI_URL || 'http://127.0.0.1:5000'}/temperatureanalyzer`, {
                 place: place?.name,
                 this_year: this_year,
                 past_span: past_span,
@@ -157,7 +174,6 @@ function Homepage() {
             if (response.data.temperature_probabilities && response.data.temperature_scores) {
                 const temperature_probabilities = response.data.temperature_probabilities;
                 const temperature_scores = response.data.temperature_scores;
-                console.log('Get temperature probabilities and scores!');
                 return [temperature_probabilities, temperature_scores];
             } else {
                 console.error('No temperature probabilities and scores in response');
@@ -168,38 +184,6 @@ function Homepage() {
         return [null, null]
     }
 
-    // TEST
-    const runtest = async () => {
-        if (!place) {
-            alert('Please select a place using the autocomplete input.');
-            return;
-        }
-        if (day_span < 1 || day_span > 60) {
-            alert('TRAVEL DAY SPAN must be between 1 and 60.');
-            return;
-        }
-        await getTestData();
-    }
-
-    const getTestData = async () => {
-        try {
-            setTestLoading(true);
-            const response = await axios.post('http://127.0.0.1:5000/test', {
-                place: place?.name,
-                lat: place?.geometry?.location?.lat(),
-                lon: place?.geometry?.location?.lng(),
-                this_year: this_year,
-                past_span: past_span,
-                day_span: day_span
-            });
-            setTestData(response.data);
-            setTestLoading(false);
-        } catch (error) {
-            console.error('Error fetching data: ', error);
-            setTestLoading(false);
-        }
-    }
-
 
     if (!isLoaded) {
         return (
@@ -208,7 +192,6 @@ function Homepage() {
             </div>
         )
     };
-
 
 
     return (
@@ -244,7 +227,9 @@ function Homepage() {
                                     type="number"
                                     className="form-control"
                                     value={day_span}
-                                    onChange={(e) => setDaySpan(Number(e.target.value))}
+                                    onChange={(e) => {
+                                        setDaySpan(parseInt(e.target.value, 10))
+                                    }}
                                     min="1"
                                     max="60"
                                 />
@@ -263,24 +248,8 @@ function Homepage() {
                                 </div>
                             </div>
 
-                            <div className="mt-4">
-                                <button className="btn btn-primary" onClick={runtest}>
-                                    RUN TEST
-                                </button>
-                            </div>
-
                         </div>
                     </div>
-
-                    {place && (
-                        <div>
-                            <h5>Place Details</h5>
-                            <p>Name: {place.name}</p>
-                            <p>Address: {place.formatted_address}</p>
-                            <p>Latitude: {place.geometry?.location?.lat()}</p>
-                            <p>Longitude: {place.geometry?.location?.lng()}</p>
-                        </div>
-                    )}
 
                 </div>
 
@@ -295,31 +264,28 @@ function Homepage() {
                             {resultloading && (
                                 <div className="d-flex justify-content-center">
                                     <div className="spinner-border" role="status">
-                                        <span className="sr-only">Loading...</span>
+                                        <span className="sr-only"></span>
                                     </div>
                                 </div>
                             )}
                             {resultData && (
                                 <div>
-                                    <h5>Data Output</h5>
-                                    <p>Input Details:</p>
+                                    <h5>Destination:</h5>
                                     <ul>
                                         <li>Place: {(resultData as any).input.place}</li>
-                                        <li>This Year: {(resultData as any).input.this_year}</li>
-                                        <li>Past Span: {(resultData as any).input.past_span}</li>
-                                        <li>Day Span: {(resultData as any).input.day_span}</li>
+                                        <li>Year: {(resultData as any).input.this_year}</li>
+                                        <li>Travel Day Span: {(resultData as any).input.day_span}</li>
                                     </ul>
-                                    <p>Output:</p>
                                     <div>
                                         {(resultData as any).recommend_date.map((period: string[], index: number) => (
                                             <div key={index}>
-                                                <p style={{ fontSize: '20px' }}>
+                                                <h5 className='mb-4' style={{ fontSize: '20px' }}>
                                                     Recommend period {index + 1}: {period[0]} ~ {period[1]}
-                                                </p>
+                                                </h5>
                                                 <div style={{ marginLeft: '20px' }}>
                                                     {(resultData as any).recommend_date_probability[index].map((dailyProb: any, idx: number) => (
                                                         <div key={idx}>
-                                                            <p>Probability of {idx % 2 === 0 ? 'rain' : 'temperature'} on {idx % 2 === 0 ? period[0] : period[1]}</p>
+                                                            <p>Probability of {idx % 2 === 0 ? 'rain' : 'temperature'} on {idx < 2 ? period[0] : period[1]}</p>
                                                             <ul>
                                                                 {Object.entries(dailyProb).map(([condition, value], conditionIdx) => (
                                                                     <li key={conditionIdx}>
@@ -338,51 +304,6 @@ function Homepage() {
 
                         </div>
                     </div>
-
-                    {/* TEST */}
-                    {testloading && (
-                        <div className="d-flex justify-content-center">
-                            <div className="spinner-border" role="status">
-                                <span className="sr-only">Loading...</span>
-                            </div>
-                        </div>
-                    )}
-                    {testData && (
-                        <div>
-                            <h5>Test Output</h5>
-                            <p>Input Details:</p>
-                            <ul>
-                                <li>Place: {(testData as any).input.place}</li>
-                                <li>This Year: {(testData as any).input.this_year}</li>
-                                <li>Past Span: {(testData as any).input.past_span}</li>
-                                <li>Day Span: {(testData as any).input.day_span}</li>
-                            </ul>
-                            <p>Output:</p>
-                            <div>
-                                {(testData as any).recommend_date.map((period: string[], index: number) => (
-                                    <div key={index}>
-                                        <p style={{ fontSize: '20px' }}>
-                                            Recommend period {index + 1}: {period[0]} ~ {period[1]}
-                                        </p>
-                                        <div style={{ marginLeft: '20px' }}>
-                                            {(testData as any).recommend_date_probability[index].map((dailyProb: any, idx: number) => (
-                                                <div key={idx}>
-                                                    <p>Probability of {idx % 2 === 0 ? 'rain' : 'temperature'} on {idx % 2 === 0 ? period[0] : period[1]}</p>
-                                                    <ul>
-                                                        {Object.entries(dailyProb).map(([condition, value], conditionIdx) => (
-                                                            <li key={conditionIdx}>
-                                                                {condition}: {Math.round(value as number * 100)}%
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )}
                     
                 </div>
 
